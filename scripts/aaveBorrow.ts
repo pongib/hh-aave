86be637dfa91492d3a5ce4c241cd6d5483321e34
pongtsu <alwayshungrytolearn@gmail.com>
2022-08-09 08:43:46 +0700

    feat: Approve erc20.

--- scripts/aaveBorrow.ts
+++ scripts/aaveBorrow.ts
@@ -1,14 +1,25 @@
+import { BigNumberish, Signer } from "ethers"
 import { ethers } from "hardhat"
-import { ILendingPoolAddressesProvider } from "../typechain-types"
-import getWeth from "./getWeth"
+import {
+  IERC20,
+  ILendingPool,
+  ILendingPoolAddressesProvider,
+} from "../typechain-types"
+import { amount, wethAddress, default as getWeth } from "./getWeth"
 
 const main = async () => {
-  // await getWeth()
-  await getLendingPool()
-  // lending pool address from LendingPoolAddressesProvider and call get funciton
+  const [deployer] = await ethers.getSigners()
+  await getWeth()
+  const lendingPool = await getLendingPool()
+  console.log(lendingPool.address)
+  // approve before deposit to contract
+  await approveErc20(wethAddress, lendingPool.address, amount, deployer)
+  // deposit
+  await lendingPool.deposit()
 }
 
 const getLendingPool = async () => {
+  // lending pool address from LendingPoolAddressesProvider and call get funciton
   const addressProvider: ILendingPoolAddressesProvider =
     await ethers.getContractAt(
       "ILendingPoolAddressesProvider",
@@ -16,11 +27,27 @@ const getLendingPool = async () => {
     )
   const lendingPoolAddress = await addressProvider.getLendingPool()
 
-  const lendingPool = await ethers.getContractAt(
+  const lendingPool: ILendingPool = await ethers.getContractAt(
     "ILendingPool",
     lendingPoolAddress
   )
   return lendingPool
+}
+
+const approveErc20 = async (
+  erc20Address: string,
+  spender: string,
+  amount: BigNumberish,
+  account: Signer
+) => {
+  const erc20: IERC20 = await ethers.getContractAt(
+    "IERC20",
+    erc20Address,
+    account
+  )
+
+  const tx = await erc20.approve(spender, amount)
+  await tx.wait()
 }
 
 main()
--- scripts/getWeth.ts
+++ scripts/getWeth.ts
@@ -1,15 +1,19 @@
 import { ethers } from "hardhat"
 import { IWeth } from "../typechain-types"
 
+const amount = ethers.utils.parseEther("1")
+
+export const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+
 const getWeth = async () => {
   const [deployer] = await ethers.getSigners()
-  const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-  const weth = (await ethers.getContractAt("IWeth", wethAddress)) as IWeth
-  const tx = await weth.deposit({ value: ethers.utils.parseEther("1") })
+  const weth: IWeth = await ethers.getContractAt("IWeth", wethAddress)
+  const tx = await weth.deposit({ value: amount })
   const txReceipt = await tx.wait()
   console.log("txReceipt", txReceipt)
   const balaceOfWeth = await weth.balanceOf(deployer.address)
-  console.log("balance ", ethers.utils.formatEther(balaceOfWeth))
+  console.log("balance weth", ethers.utils.formatEther(balaceOfWeth))
 }
 
 export default getWeth
+export { amount }
